932e3895114608b10f603eee4beedf6896340f4d
Kirti Padhi <102805180+Kirti-kn@users.noreply.github.com>
2023-03-30 16:19:22 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,99 @@
+import numpy as np
+from flask import Flask, request, jsonify, render_template
+import pickle
+
+app = Flask(__name__)
+model = pickle.load(open('model.pkl', 'rb'))
+
+l1=['back_pain','constipation','abdominal_pain','diarrhoea','mild_fever','yellow_urine',
+'yellowing_of_eyes','acute_liver_failure','fluid_overload','swelling_of_stomach',
+'swelled_lymph_nodes','malaise','blurred_and_distorted_vision','phlegm','throat_irritation',
+'redness_of_eyes','sinus_pressure','runny_nose','congestion','chest_pain','weakness_in_limbs',
+'fast_heart_rate','pain_during_bowel_movements','pain_in_anal_region','bloody_stool',
+'irritation_in_anus','neck_pain','dizziness','cramps','bruising','obesity','swollen_legs',
+'swollen_blood_vessels','puffy_face_and_eyes','enlarged_thyroid','brittle_nails',
+'swollen_extremeties','excessive_hunger','extra_marital_contacts','drying_and_tingling_lips',
+'slurred_speech','knee_pain','hip_joint_pain','muscle_weakness','stiff_neck','swelling_joints',
+'movement_stiffness','spinning_movements','loss_of_balance','unsteadiness',
+'weakness_of_one_body_side','loss_of_smell','bladder_discomfort','foul_smell_of urine',
+'continuous_feel_of_urine','passage_of_gases','internal_itching','toxic_look_(typhos)',
+'depression','irritability','muscle_pain','altered_sensorium','red_spots_over_body','belly_pain',
+'abnormal_menstruation','dischromic _patches','watering_from_eyes','increased_appetite','polyuria','family_history','mucoid_sputum',
+'rusty_sputum','lack_of_concentration','visual_disturbances','receiving_blood_transfusion',
+'receiving_unsterile_injections','coma','stomach_bleeding','distention_of_abdomen',
+'history_of_alcohol_consumption','fluid_overload','blood_in_sputum','prominent_veins_on_calf',
+'palpitations','painful_walking','pus_filled_pimples','blackheads','scurring','skin_peeling',
+'silver_like_dusting','small_dents_in_nails','inflammatory_nails','blister','red_sore_around_nose',
+'yellow_crust_ooze']
+
+#List of Diseases is listed in list disease.
+
+disease=['Fungal infection','Allergy','GERD','Chronic cholestasis','Drug Reaction',
+'Peptic ulcer diseae','AIDS','Diabetes','Gastroenteritis','Bronchial Asthma','Hypertension',
+' Migraine','Cervical spondylosis',
+'Paralysis (brain hemorrhage)','Jaundice','Malaria','Chicken pox','Dengue','Typhoid','hepatitis A',
+'Hepatitis B','Hepatitis C','Hepatitis D','Hepatitis E','Alcoholic hepatitis','Tuberculosis',
+'Common Cold','Pneumonia','Dimorphic hemmorhoids(piles)',
+'Heartattack','Varicoseveins','Hypothyroidism','Hyperthyroidism','Hypoglycemia','Osteoarthristis',
+'Arthritis','(vertigo) Paroymsal  Positional Vertigo','Acne','Urinary tract infection','Psoriasis',
+'Impetigo']
+
+l2=[]
+
+for i in range(0,len(l1)):
+    l2.append(0)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    '''
+    For rendering results on HTML GUI
+    '''
+    sym1=request.form['s1']
+    sym2=request.form['s2']
+    sym3=request.form['s3']
+    sym4=request.form['s4']
+    final_features=[]
+    final_features.append(sym1)
+    final_features.append(sym2)
+    final_features.append(sym3)
+    final_features.append(sym4)
+    #final_features = [np.array(int_features)]
+
+    for k in range(0,len(l1)):
+        for z in final_features:
+            if(z==l1[k]):
+                l2[k]=1
+    inputtest = [l2]
+    predict = model.predict(inputtest)
+    #predicted=predict[0]
+    # h='no'
+    # for a in range(0,len(disease)):
+    #     if(predicted == a):
+    #         h='yes'
+    #         break
+    # if (h=='yes'):
+    #     output=(disease[a])
+    # else:
+    #     output=("Not found")
+    output=disease[predict[0]]
+
+
+    return render_template('result.html', prediction_text='You may be suffering from: {}'.format(output))
+
+# @app.route('/predict_api',methods=['POST'])
+# def predict_api():
+#     '''
+#     For direct API calls trought request
+#     '''
+#     data = request.get_json(force=True)
+#     prediction = model.predict([np.array(list(data.values()))])
+
+#     output = prediction[0]
+#     return jsonify(output)
+
+if __name__ == "__main__":
+    app.run(debug=True)
